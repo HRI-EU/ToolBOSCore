HRI-EU/ToolBOSCore

commit f311b4f145003c7fc4af5a597ce8eb51cae5b266

[TBCORE-2244] Klocwork: show progress output in verbose mode

include/ToolBOSCore/SoftwareQuality/Rules.py
@@ -1578,7 +1578,7 @@ once in a while inspect your code using Klocwork.'''
         logging.debug( 'performing source code analysis using Klocwork' )
         passed = 0
         failed = 0
-        output = io.StringIO()
+        output = io.StringIO() if Any.getDebugLevel() < 3 else None
         error  = False
         kwDir  = tempfile.mkdtemp( prefix='klocwork-' )
 

commit ec605d798e48480516d059c204d654a3a2b8cc7a

[TBCORE-2264] reverted f311b4f145003c7fc4af5a597ce8eb51cae5b266 which introduced a Klocwork regression

include/ToolBOSCore/SoftwareQuality/Rules.py
@@ -1579,7 +1579,7 @@ once in a while inspect your code using Klocwork.'''
         logging.debug( 'performing source code analysis using Klocwork' )
         passed = 0
         failed = 0
-        output = io.StringIO() if Any.getDebugLevel() < 3 else None
+        output = io.StringIO()
         error  = False
         kwDir  = tempfile.mkdtemp( prefix='klocwork-' )
 
@@ -1587,6 +1587,7 @@ once in a while inspect your code using Klocwork.'''
         # failed/passed files like this, instead later on we parse the output.
 
         try:
+            logging.info( 'performing source code analysis using Klocwork, this may take some time...' )
             Klocwork.createLocalProject( kwDir, output, output )
             Klocwork.codeCheck( kwDir, output, output )
 