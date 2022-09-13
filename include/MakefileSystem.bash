72c76e0442938a390ba7ddd7662c84200cbedc7a
Marijke Stein <marijke.stein@honda-ri.de>
2022-09-13 08:06:48 +0200

    [#13] MakefileSystem.bash: dropped support of reading PROJECT_REVISION from SVN

--- include/MakefileSystem.bash
+++ include/MakefileSystem.bash
@@ -80,15 +80,9 @@ PROJECT_VERSION=$(basename "${PWD}")                # 2 digits
 PROJECT_VERSION_MAJOR=$(echo "${PROJECT_VERSION}" | awk -F. '{ print $1 }')
 PROJECT_VERSION_MINOR=$(echo "${PROJECT_VERSION}" | awk -F. '{ print $2 }')
 
-
-if [[ -z "${PROJECT_REVISION}" ]]
+if [[ -z ${PROJECT_CATEGORY+x} ]]
 then
-  PROJECT_REVISION=$(svn info | awk -F': ' '/Revision/ { print $2 }')
-fi
-
-if [[ -z "${PROJECT_PATCHLEVEL_VERSION}" ]]
-then
-  PROJECT_PATCHLEVEL_VERSION=${PROJECT_VERSION}.${PROJECT_REVISION}  # 3 digits
+    PROJECT_CATEGORY=""
 fi
 
 if [[ -z "${PROJECT_CATEGORY}" && -r "CMakeLists.txt" ]]
@@ -100,8 +94,7 @@ fi
 
 CANONICAL_PATH=${PROJECT_CATEGORY}/${PROJECT_NAME}/${PROJECT_VERSION}
 PROJECT_ROOT=${SIT}/${CANONICAL_PATH}
-PROJECT_ROOT_FULL=${PROJECT_ROOT}.${PROJECT_REVISION}
-INSTALL_ROOT=${PROJECT_ROOT_FULL}
+INSTALL_ROOT=${PROJECT_ROOT}
 SRC_TARBALL_NAME=sources.tar.bz2
 BIN_TARBALL_NAME=package.tar.bz2
 SOURCES_DIR=sources
