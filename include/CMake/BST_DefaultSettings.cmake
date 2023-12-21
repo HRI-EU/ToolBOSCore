174655784b111a061d50f8433817af5b0ae15dd9
Marijke Stein <marijke.stein@honda-ri.de>
2023-12-21 15:13:53 +0100

    [#31] :sparkles: support Ubuntu 22.04 LTS ("jammy64")

--- include/CMake/BST_DefaultSettings.cmake
+++ include/CMake/BST_DefaultSettings.cmake
@@ -140,10 +140,11 @@ if("$ENV{MAKEFILE_PLATFORM}" STREQUAL "")
     message(FATAL_ERROR "Please set the MAKEFILE_PLATFORM environment variable.")
 
 elseif("$ENV{MAKEFILE_PLATFORM}" STREQUAL "bionic64" OR
-       "$ENV{MAKEFILE_PLATFORM}" STREQUAL "focal64")
+       "$ENV{MAKEFILE_PLATFORM}" STREQUAL "focal64"  OR
+       "$ENV{MAKEFILE_PLATFORM}" STREQUAL "jammy64")
 
-    set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} ${BST_DEFAULT_FLAGS_LINUX} -mtune=sandybridge -m64 -std=c99")
-    set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} ${BST_DEFAULT_FLAGS_LINUX} -mtune=sandybridge -m64")
+    set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} ${BST_DEFAULT_FLAGS_LINUX} -m64 -std=c99")
+    set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} ${BST_DEFAULT_FLAGS_LINUX} -m64")
     set(BST_DEFAULT_DEFINES     "-D__64BIT__ -D__linux__")
     add_definitions(${BST_DEFAULT_DEFINES})
 
